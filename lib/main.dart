8b22fcbfb9f8a902981670b15208f75b027d5869
ZyaBlik <58951048+DarkDan25@users.noreply.github.com>
2025-10-03 21:04:22 +0300

    Улучшен виджет для ввода ФМО

--- lib/main.dart
+++ lib/main.dart
@@ -63,13 +63,15 @@ class TypeName extends StatefulWidget{
 
 class _TypeNameState extends State<TypeName>{
   final myController = TextEditingController();
+  final myController2 = TextEditingController();
+  final myController3 = TextEditingController();
 
   @override
   Widget build(BuildContext context){
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text("1. Введи имя"),
+        title: Text("1. Введи ФИО"),
       ),
       body: Center(
         child: Column(
@@ -77,9 +79,13 @@ class _TypeNameState extends State<TypeName>{
           children: <Widget>[
             Text("Введи имя"),
             TextField(controller: myController),
+            Text("Введи Фамилию"),
+            TextField(controller: myController2),
+            Text("Введи Отчество"),
+            TextField(controller: myController3),
             ElevatedButton(
                 onPressed:() {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) => TypeAge(name: myController.text,)));
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => TypeAge(name: "${myController2.text} ${myController.text} ${myController3.text}",)));
                   },
                 child: Text("Далее"))
           ],
