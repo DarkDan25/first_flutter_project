648345a772a76533fe4a10c0cc3ea5533e1a4967
ZyaBlik <58951048+DarkDan25@users.noreply.github.com>
2025-10-17 22:24:10 +0300

    Создана модель данных записи

---
+++ lib/models/appointment.dart
@@ -0,0 +1,13 @@
+class Appointment {
+  final String doctorName;
+  final String specialty;
+  final String date;
+  final String time;
+
+  Appointment({
+    required this.doctorName,
+    required this.specialty,
+    required this.date,
+    required this.time,
+  });
+}
