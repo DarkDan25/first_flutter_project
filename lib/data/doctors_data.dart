26bad00f83fabc5112d7e3b00999d1b216ae0d2f
Zyablik <elektrosha88@gmail.com>
2025-11-01 02:56:55 +0300

    Cleaning

--- lib/data/doctors_data.dart
+++ lib/data/doctors_data.dart
@@ -6,31 +6,26 @@ class DoctorsData {
       name: 'Иванов И.И.',
       specialty: 'Кардиолог',
       imageUrl: 'https://th.bing.com/th/id/R.9dae0eedbe8906edc82842714e98f5d0?rik=9M7OqrjRzNNKUQ&riu=http%3a%2f%2fstatic.wixstatic.com%2fmedia%2f095632_461ab111988a4121b8e53331070b67d4~mv2.jpg&ehk=HwAXIIkghyQUyz0GN9Pv5eOmGc0w04l9tOERr%2bYxIqM%3d&risl=&pid=ImgRaw&r=0',
-      //localAsset: 'assets/images/cardiologist.png',
     ),
     Doctor(
       name: 'Петрова А.Д.',
       specialty: 'Невролог',
       imageUrl: 'https://th.bing.com/th/id/R.a286f386db62bda990679447651a2e26?rik=l%2focmVKfl%2fc2ew&riu=http%3a%2f%2fstatic.scientificamerican.com%2fsciam%2fcache%2ffile%2fF6FF3DF2-A99D-4BE0-99595C9E4717FEF4_source.jpg&ehk=QCq1Bg063T1WlmT7fmxnFeL6dMWGlM56jqjUhs2fvkg%3d&risl=&pid=ImgRaw&r=0',
-      //localAsset: 'assets/images/neurologist.png',
     ),
     Doctor(
       name: 'Сидоров Е.В.',
       specialty: 'Терапевт',
       imageUrl: 'https://th.bing.com/th/id/R.6313a0b7f4b9d06b42a864b3eabddf86?rik=Jy6Bm2vQiamdtw&riu=http%3a%2f%2ffactinate.com%2fwp-content%2fuploads%2f2019%2f01%2fshutterstock_718159351.jpg&ehk=ypmvIyzfCR4PJTukwWsWnwhEFewIONfv5l9A3ZkPcV8%3d&risl=&pid=ImgRaw&r=0',
-      //localAsset: 'assets/images/therapist.png',
     ),
     Doctor(
       name: 'Кузнецова К.Н.',
       specialty: 'Стоматолог',
       imageUrl: 'https://cdnn21.img.ria.ru/images/07e7/04/18/1867278481_185:24:789:628_1920x0_80_0_0_4e0a087eda1ddddedee0ca48772748c8.jpg',
-      //localAsset: 'assets/images/dentist.png',
     ),
     Doctor(
       name: 'Кузин О.М.',
       specialty: 'Хирург',
       imageUrl: 'https://tse4.mm.bing.net/th/id/OIP.ncZkfwsLevJF1Lq69gKsSAHaE8?w=1023&h=682&rs=1&pid=ImgDetMain&o=7&rm=3',
-      //localAsset: 'assets/images/surgeon.png',
     ),
   ];
 
