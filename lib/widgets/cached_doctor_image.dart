2f59635d5c3fc2ba756533c469373174a7837601
ZyaBlik <58951048+DarkDan25@users.noreply.github.com>
2025-10-31 22:17:37 +0300

    Добавлен виджет для изображения врача

---
+++ lib/widgets/cached_doctor_image.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:first_flutter_project/widgets/cached_doctor_image.dart';
+
+class CachedDoctorImage extends StatelessWidget {
+  final String imageUrl;
+  final String localAsset;
+  final double width;
+  final double height;
+
+  const CachedDoctorImage({
+    Key? key,
+    required this.imageUrl,
+    required this.localAsset,
+    this.width = 60,
+    this.height = 60,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return CachedNetworkImage(
+      imageUrl: imageUrl,
+      width: width,
+      height: height,
+      fit: BoxFit.cover,
+      placeholder: (context, url) => CircleAvatar(
+        radius: width / 2,
+        backgroundColor: Colors.grey[300],
+        child: Icon(Icons.medical_services, color: Colors.grey[600]),
+      ),
+      errorWidget: (context, url, error) => CircleAvatar(
+        radius: width / 2,
+        backgroundColor: Colors.grey[300],
+        child: Image.asset(
+          localAsset,
+          width: width - 10,
+          height: height - 10,
+        ),
+      ),
+      imageBuilder: (context, imageProvider) => CircleAvatar(
+        radius: width / 2,
+        backgroundImage: imageProvider,
+      ),
+    );
+  }
+}
