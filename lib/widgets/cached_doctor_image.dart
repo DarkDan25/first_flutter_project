e44234b4c5061b8b9fcf94080834a416e241ee97
ZyaBlik <58951048+DarkDan25@users.noreply.github.com>
2025-10-31 22:21:01 +0300

    Обновлены зависимости

--- lib/widgets/cached_doctor_image.dart
+++ lib/widgets/cached_doctor_image.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:first_flutter_project/widgets/cached_doctor_image.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
 class CachedDoctorImage extends StatelessWidget {
   final String imageUrl;
