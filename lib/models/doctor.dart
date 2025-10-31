14b6110ceb1db9c82790e7253cabdee9d6885d7c
ZyaBlik <58951048+DarkDan25@users.noreply.github.com>
2025-10-31 22:12:02 +0300

    Добавлена модель для доктора

---
+++ lib/models/doctor.dart
@@ -0,0 +1,13 @@
+class Doctor {
+  final String name;
+  final String specialty;
+  final String imageUrl;
+  final String localAsset;
+
+  Doctor({
+    required this.name,
+    required this.specialty,
+    required this.imageUrl,
+    required this.localAsset,
+  });
+}
