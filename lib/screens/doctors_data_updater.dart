5fb697581684afed1784c4ff2c8c74d053e08463
ZyaBlik <58951048+DarkDan25@users.noreply.github.com>
2025-12-13 01:28:31 +0300

    Code cleaning

--- lib/screens/doctors_data_updater.dart
+++ lib/screens/doctors_data_updater.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:first_flutter_project/bloc/doctors_management/doctors_management_bloc.dart';
-import 'package:first_flutter_project/bloc/make_appointment/make_appointment_bloc.dart';
 
 class DoctorsDataUpdater extends StatelessWidget {
   final Widget child;
@@ -13,8 +12,6 @@ class DoctorsDataUpdater extends StatelessWidget {
     return BlocListener<DoctorsManagementBloc, DoctorsManagementState>(
       listener: (context, state) {
         if (state is DoctorsLoaded) {
-          // При изменении списка врачей можно добавить дополнительные действия
-          // Например, обновить другие блоки, которые зависят от списка врачей
         }
       },
       child: child,
