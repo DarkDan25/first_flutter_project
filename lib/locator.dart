9d654bee92f9487c876b21887b7587bbb8836f87
ZyaBlik <58951048+DarkDan25@users.noreply.github.com>
2025-12-13 01:42:29 +0300

    Structure cleaning

--- lib/locator.dart
+++
@@ -1,8 +0,0 @@
-import 'package:get_it/get_it.dart';
-import 'package:first_flutter_project/services/app_state_service.dart';
-
-final getIt = GetIt.instance;
-
-void setupLocator() {
-  getIt.registerLazySingleton<AppStateService>(() => AppStateService());
-}
